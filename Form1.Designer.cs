0e0125fb92fb89cd1846bae6e99875a03c3518f7
Hamzah Alyami <Hamza.Alyami@outlook.com>
2023-01-01 21:28:57 +0300

    new v

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,50 @@ namespace Test101
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.IsLink = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(754, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "Check for Updates";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,60 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace Test101
 {
     public partial class Form1 : Form
     {
+        UpdateManager manager;
         public Form1()
         {
             InitializeComponent();
+
+            CheckforUpdates();
+        }
+
+
+        private async Task CheckforUpdates()
+        {
+            manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/hy043/Test101");
+          
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo VerisonInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            this.Text += $" v.{VerisonInfo.FileVersion}";
+
+        }
+
+        private async void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            var updateinfo = await manager.CheckForUpdate();
+            if (updateinfo.ReleasesToApply.Count > 0)
+            {
+                DialogResult dr = ShowDialog();
+                if (dr == DialogResult.Yes)
+                {
+                    await manager.UpdateApp();
+                    MessageBox.Show("updated");
+                }
+                else if (dr == DialogResult.No)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("you're up to date");
+            }
         }
     }
 }
